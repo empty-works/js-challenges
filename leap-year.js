be8e43fa8ae7d6f0bbf77e9a387bfb20b3f9bb57
empty-works <pama.mark@gmail.com>
2020-06-08 23:45:44 +0200

    Function that determines if a year is a leap year or not.

--- leap-year.js
+++ leap-year.js
@@ -6,6 +6,20 @@ const rl = readline.createInterface({
  
 rl.question("Enter a year: ", (year) => {
 
+	
 	rl.close();
 });
 
+function isLeapYear(year) {
+	var isLeap = false;
+	if(year % 4 === 0) {
+		isLeap = true;
+		if(year % 100 === 0) {
+			isLeap = false;
+			if(year % 400 === 0) {
+				isLeap = true;
+			}
+		}
+	}
+	return isLeap;
+}
