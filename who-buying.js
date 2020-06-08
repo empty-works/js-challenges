6c964a57b0ff13557e06fcce91ab981f42cebd7c
empty-works <pama.mark@gmail.com>
2020-06-08 23:58:04 +0200

    Finished the who is buying lunch challenge.

--- who-buying.js
+++ who-buying.js
@@ -1,3 +1,4 @@
 var candidates = ["Angela", "Tom", "Harry", "Lunestra", "Choalynn", "Markus"];
 
-
+var rand = Math.floor(Math.random() * candidates.length);
+console.log(candidates[rand] + " is buying lunch today!");
