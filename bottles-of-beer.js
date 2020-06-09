da9eb34660cdc92f0383d79039fc12388fd1814f
empty-works <pama.mark@gmail.com>
2020-06-09 02:58:30 +0200

    Completed bottles of beer challenge.

---
+++ bottles-of-beer.js
@@ -0,0 +1,6 @@
+var numBottles = 99;
+
+while(numBottles > 0) {
+	console.log(numBottles + " bottles of beer on the wall, " + numBottles + " bottles of beer.");
+	console.log("Take one down and pass it around, " + --numBottles + " bottles of beer on the wall");
+}
