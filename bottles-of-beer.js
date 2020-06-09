79cc77950ba979e97d4d11f3a2cc19b60306f109
empty-works <pama.mark@gmail.com>
2020-06-09 03:11:52 +0200

    Updated bottles of beer program by making it so the lyrics change when the number goes down to one.

--- bottles-of-beer.js
+++ bottles-of-beer.js
@@ -1,6 +1,8 @@
 var numBottles = 99;
+var bottlesWord = "bottles";
 
 while(numBottles > 0) {
-	console.log(numBottles + " bottles of beer on the wall, " + numBottles + " bottles of beer.");
-	console.log("Take one down and pass it around, " + --numBottles + " bottles of beer on the wall");
+	if(numBottles === 1) {bottlesWord = "bottle";}	
+	console.log(numBottles + " " + bottlesWord + " of beer on the wall, " + numBottles + " " + bottlesWord + " of beer.");
+	console.log("Take one down and pass it around, " + --numBottles + " " + bottlesWord + " of beer on the wall");
 }
