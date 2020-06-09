473480507bca79a6e9a00b733e7f4044efad3cf4
empty-works <pama.mark@gmail.com>
2020-06-09 03:26:54 +0200

    Refactored how the lyrics change with only one bottle of beer.

--- bottles-of-beer.js
+++ bottles-of-beer.js
@@ -4,12 +4,6 @@ var bottlesWord = "bottles";
 while(numBottles > 0) {
 	console.log(numBottles + " " + bottlesWord + " of beer on the wall, " + numBottles + " " + bottlesWord + " of beer.");
 	--numBottles;
-	bottlesWord = getBottleWord(numBottles);
+	if(numBottles === 1) {bottlesWord = "bottle";}
 	console.log("Take one down and pass it around, " + numBottles + " " + bottlesWord + " of beer on the wall");
-}
-
-function getBottleWord(numBottles) {
-	if(numBottles === 1) {
-		return "bottle";
-	}
 }
