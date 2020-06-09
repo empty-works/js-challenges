4590a24a885aa0dd6a16150d22cc6219a56c34e1
empty-works <pama.mark@gmail.com>
2020-06-09 04:18:41 +0200

    Added first two cases of the sequence.

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,11 @@
-console.log("Check with 1: " + getFibSequence(1));
+console.log("Check with 1: [" + getFibSequence(1) + "]");
+console.log("Check with 2: [" + getFibSequence(2) + "]");
 
 function getFibSequence(num) {
-	var fibArray = [];
+	var fibArray = [0];
+
+	if(num === 2) {
+		fibArray.push(1);
+	}
+	return fibArray;
 }
