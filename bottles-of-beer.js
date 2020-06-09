e26349281a6355ade0c35d287b1b6c253072dd9d
empty-works <pama.mark@gmail.com>
2020-06-09 03:36:04 +0200

    Updated beer challenge to change the lyrics when there are zero bottles of beer on the wall.

--- bottles-of-beer.js
+++ bottles-of-beer.js
@@ -5,5 +5,14 @@ while(numBottles > 0) {
 	console.log(numBottles + " " + bottlesWord + " of beer on the wall, " + numBottles + " " + bottlesWord + " of beer.");
 	--numBottles;
 	if(numBottles === 1) {bottlesWord = "bottle";}
-	console.log("Take one down and pass it around, " + numBottles + " " + bottlesWord + " of beer on the wall");
+	console.log("Take one down and pass it around, " + getLyrics(numBottles, bottlesWord) + " of beer on the wall.");
+}
+
+function getLyrics(numBottles, bottlesWord) {
+	if(numBottles === 0) {
+		return "no more bottles";
+	}
+	else {
+		return numBottles + " " + bottlesWord;
+	}
 }
